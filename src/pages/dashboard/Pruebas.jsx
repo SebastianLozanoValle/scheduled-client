a6f5059a244d22dfd59bc83fb8d8f7d68fd89a3c
SebastianLozanoValle <cesarloco2003@hotmail.com>
2023-12-16 01:39:24 -0500

    intentando integrar shadcn

---
+++ src/pages/dashboard/Pruebas.jsx
@@ -0,0 +1,12 @@
+// import { Button } from "@/components/ui/button"
+
+export const Pruebas = () => {
+    return (
+        <>
+            <h1 className="ml-72 text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            {/* <Button>Click me!</Button> */}
+        </>
+    )
+}
