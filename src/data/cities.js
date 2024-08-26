47679154a5e9e6645df642a8d43f8c2c0a6740ef
SebastianLozanoValle <cesarloco2003@hotmail.com>
2024-08-26 03:49:31 -0500

    actualizado a las ciudades de colombia

--- src/data/cities.js
+++ src/data/cities.js
@@ -1,494 +1,178 @@
-let data = `San José, Carmen
-San José, Merced
-San José, Hospital
-San José, Catedral
-San José, Zapote
-San José, San Francisco de Dos Ríos
-San José, Uruca
-San José, Mata Redonda
-San José, Pavas
-San José, Hatillo
-San José, San Sebastián
-Escazú, Escazú
-Escazú, San Antonio
-Escazú, San Rafael
-Desamparados, Desamparados
-Desamparados, San Miguel
-Desamparados, San Juan de Dios
-Desamparados, San Rafael Arriba
-Desamparados, San Antonio
-Desamparados, Frailes
-Desamparados, Patarrá
-Desamparados, San Cristóbal
-Desamparados, Rosario
-Desamparados, Damas
-Desamparados, San Rafael Abajo
-Desamparados, Gravilias
-Desamparados, Los Guido
-Puriscal, Santiago
-Puriscal, Mercedes Sur
-Puriscal, Barbacoas
-Puriscal, Grifo Alto
-Puriscal, San Rafael
-Puriscal, Candelarita
-Puriscal, Desamparaditos
-Puriscal, San Antonio
-Puriscal, Chires
-Tarrazú, San Marcos
-Tarrazú, San Lorenzo
-Tarrazú, San Carlos
-Aserrí, Aserrí
-Aserrí, Tarbaca
-Aserrí, Vuelta de Jorco
-Aserrí, San Gabriel
-Aserrí, Legua
-Aserrí, Monterrey
-Aserrí, Salitrillos
-Mora, Colón
-Mora, Guayabo
-Mora, Tabarcia
-Mora, Piedras Negras
-Mora, Picagres
-Mora, Jaris
-Mora, Quitirrisí
-Goicoechea, Guadalupe
-Goicoechea, San Francisco
-Goicoechea, Calle Blancos
-Goicoechea, Mata de Plátano
-Goicoechea, Ipís
-Goicoechea, Rancho Redondo
-Goicoechea, Purral
-Santa Ana, Santa Ana
-Santa Ana, Salitral
-Santa Ana, Pozos
-Santa Ana, Uruca
-Santa Ana, Piedades
-Santa Ana, Brasil
-Alajuelita, Alajuelita
-Alajuelita, San Josecito
-Alajuelita, San Antonio
-Alajuelita, Concepción
-Alajuelita, San Felipe
-Vázquez de Coronado, San Isidro
-Vázquez de Coronado, San Rafael
-Vázquez de Coronado, Dulce Nombre de Jesús
-Vázquez de Coronado, Patalillo
-Vázquez de Coronado, Cascajal
-Acosta, San Ignacio
-Acosta, Guaitil
-Acosta, Palmichal
-Acosta, Cangrejal
-Acosta, Sabanillas
-Tibás, San Juan
-Tibás, Cinco Esquinas
-Tibás, Anselmo Llorente
-Tibás, León XIII
-Tibás, Colima
-Moravia, San Vicente
-Moravia, San Jerónimo
-Moravia, La Trinidad
-Montes de Oca, San Pedro
-Montes de Oca, Sabanilla
-Montes de Oca, Mercedes
-Montes de Oca, San Rafael
-Turrubares, San Pablo
-Turrubares, San Pedro
-Turrubares, San Juan de Mata
-Turrubares, San Luis
-Turrubares, Carara
-Dota, Santa María
-Dota, Jardín
-Dota, Copey
-Curridabat, Curridabat
-Curridabat, Granadilla
-Curridabat, Sánchez
-Curridabat, Tirrases
-Pérez Zeledón, San Isidro de El General
-Pérez Zeledón, El General
-Pérez Zeledón, Daniel Flores
-Pérez Zeledón, Rivas
-Pérez Zeledón, San Pedro
-Pérez Zeledón, Platanares
-Pérez Zeledón, Pejibaye
-Pérez Zeledón, Cajón
-Pérez Zeledón, Barú
-Pérez Zeledón, Río Nuevo
-Pérez Zeledón, Páramo
-Pérez Zeledón, La Amistad
-León Cortés Castro, San Pablo
-León Cortés Castro, San Andrés
-León Cortés Castro, Llano Bonito
-León Cortés Castro, San Isidro
-León Cortés Castro, Santa Cruz
-León Cortés Castro, San Antonio
-Alajuela, Alajuela
-Alajuela, San José
-Alajuela, Carrizal
-Alajuela, San Antonio
-Alajuela, Guácima
-Alajuela, San Isidro
-Alajuela, Sabanilla
-Alajuela, San Rafael
-Alajuela, Río Segundo
-Alajuela, Desamparados
-Alajuela, Turrúcares
-Alajuela, Tambor
-Alajuela, Garita
-Alajuela, Sarapiquí
-San Ramón, San Ramón
-San Ramón, Santiago
-San Ramón, San Juan
-San Ramón, Piedades Norte
-San Ramón, Piedades Sur
-San Ramón, San Rafael
-San Ramón, San Isidro
-San Ramón, Ángeles
-San Ramón, Alfaro
-San Ramón, Volio
-San Ramón, Concepción
-San Ramón, Zapotal
-San Ramón, Peñas Blancas
-San Ramón, San Lorenzo
-Grecia, Grecia
-Grecia, San Isidro
-Grecia, San José
-Grecia, San Roque
-Grecia, Tacares
-Grecia, Puente de Piedra
-Grecia, Bolívar
-San Mateo, San Mateo
-San Mateo, Desmonte
-San Mateo, Jesús María
-San Mateo, Labrador
-Atenas, Atenas
-Atenas, Jesús
-Atenas, Mercedes
-Atenas, San Isidro
-Atenas, Concepción
-Atenas, San José
-Atenas, Santa Eulalia
-Atenas, Escobal
-Naranjo, Naranjo
-Naranjo, San Miguel
-Naranjo, San José
-Naranjo, Cirrí Sur
-Naranjo, San Jerónimo
-Naranjo, San Juan
-Naranjo, El Rosario
-Naranjo, Palmitos
-Palmares, Palmares
-Palmares, Zaragoza
-Palmares, Buenos Aires
-Palmares, Santiago
-Palmares, Candelaria
-Palmares, Esquipulas
-Palmares, La Granja
-Poás, San Pedro
-Poás, San Juan
-Poás, San Rafael
-Poás, Carrillos
-Poás, Sabana Redonda
-Orotina, Orotina
-Orotina, El Mastate
-Orotina, Hacienda Vieja
-Orotina, Coyolar
-Orotina, La Ceiba
-San Carlos, Quesada
-San Carlos, Florencia
-San Carlos, Buenavista
-San Carlos, Aguas Zarcas
-San Carlos, Venecia
-San Carlos, Pital
-San Carlos, La Fortuna
-San Carlos, La Tigra
-San Carlos, La Palmera
-San Carlos, Venado
-San Carlos, Cutris
-San Carlos, Monterrey
-San Carlos, Pocosol
-Zarcero, Zarcero
-Zarcero, Laguna
-Zarcero, Tapesco
-Zarcero, Guadalupe
-Zarcero, Palmira
-Zarcero, Zapote
-Zarcero, Brisas
-Sarchí, Sarchí Norte
-Sarchí, Sarchí Sur
-Sarchí, Toro Amarillo
-Sarchí, San Pedro
-Sarchí, Rodríguez
-Upala, Upala
-Upala, Aguas Claras
-Upala, San José
-Upala, Bijagua
-Upala, Delicias
-Upala, Dos Ríos
-Upala, Yolillal
-Upala, Canalete
-Los Chiles, Los Chiles
-Los Chiles, Caño Negro
-Los Chiles, El Amparo
-Los Chiles, San Jorge
-Guatuso, San Rafael
-Guatuso, Buenavista
-Guatuso, Cote
-Guatuso, Katira
-Río Cuarto, Río Cuarto
-Río Cuarto, Santa Rita
-Río Cuarto, Santa Isabel
-Cartago, Oriental
-Cartago, Occidental
-Cartago, Carmen
-Cartago, San Nicolás
-Cartago, Aguacaliente
-Cartago, Guadalupe
-Cartago, Corralillo
-Cartago, Tierra Blanca
-Cartago, Dulce Nombre
-Cartago, Llano Grande
-Cartago, Quebradilla
-Paraíso, Paraíso
-Paraíso, Santiago
-Paraíso, Orosi
-Paraíso, Cachí
-Paraíso, Llanos de Santa Lucía
-Paraíso, Birrisito
-La Unión, Tres Ríos
-La Unión, San Diego
-La Unión, San Juan
-La Unión, San Rafael
-La Unión, Concepción
-La Unión, Dulce Nombre
-La Unión, San Ramón
-La Unión, Río Azul
-Jiménez, Juan Viñas
-Jiménez, Tucurrique
-Jiménez, Pejibaye
-Jiménez, La Victoria
-Turrialba, Turrialba
-Turrialba, La Suiza
-Turrialba, Peralta
-Turrialba, Santa Cruz
-Turrialba, Santa Teresita
-Turrialba, Pavones
-Turrialba, Tuis
-Turrialba, Tayutic
-Turrialba, Santa Rosa
-Turrialba, Tres Equis
-Turrialba, La Isabel
-Turrialba, Chirripó
-Alvarado, Pacayas
-Alvarado, Cervantes
-Alvarado, Capellades
-Oreamuno, San Rafael
-Oreamuno, Cot
-Oreamuno, Potrero Cerrado
-Oreamuno, Cipreses
-Oreamuno, Santa Rosa
-El Guarco, El Tejar
-El Guarco, San Isidro
-El Guarco, Tobosi
-El Guarco, Patio de Agua
-Heredia, Heredia
-Heredia, Mercedes
-Heredia, San Francisco
-Heredia, Ulloa
-Heredia, Varablanca
-Barva, Barva
-Barva, San Pedro
-Barva, San Pablo
-Barva, San Roque
-Barva, Santa Lucía
-Barva, San José de la Montaña
-Santo Domingo, Santo Domingo
-Santo Domingo, San Vicente
-Santo Domingo, San Miguel
-Santo Domingo, Paracito
-Santo Domingo, Santo Tomás
-Santo Domingo, Santa Rosa
-Santo Domingo, Tures
-Santo Domingo, Pará
-Santa Bárbara, Santa Bárbara
-Santa Bárbara, San Pedro
-Santa Bárbara, San Juan
-Santa Bárbara, Jesús
-Santa Bárbara, Santo Domingo
-Santa Bárbara, Purabá
-San Rafael, San Rafael
-San Rafael, San Josecito
-San Rafael, Santiago
-San Rafael, Ángeles
-San Rafael, Concepción
-San Isidro, San Isidro
-San Isidro, San José
-San Isidro, Concepción
-San Isidro, San Francisco
-Belén, San Antonio
-Belén, La Ribera
-Belén, La Asunción
-Flores, San Joaquín
-Flores, Barrantes
-Flores, Llorente
-San Pablo, San Pablo
-San Pablo, Rincón de Sabanilla
-Sarapiquí, Puerto Viejo
-Sarapiquí, La Virgen
-Sarapiquí, Las Horquetas
-Sarapiquí, Llanuras del Gaspar
-Sarapiquí, Cureña
-Liberia, Liberia
-Liberia, Cañas Dulces
-Liberia, Mayorga
-Liberia, Nacascolo
-Liberia, Curubandé
-Nicoya, Nicoya
-Nicoya, Mansión
-Nicoya, San Antonio
-Nicoya, Quebrada Honda
-Nicoya, Sámara
-Nicoya, Nosara
-Nicoya, Belén de Nosarita
-Santa Cruz, Santa Cruz
-Santa Cruz, Bolsón
-Santa Cruz, Veintisiete de Abril
-Santa Cruz, Tempate
-Santa Cruz, Cartagena
-Santa Cruz, Cuajiniquil
-Santa Cruz, Diriá
-Santa Cruz, Cabo Velas
-Santa Cruz, Tamarindo
-Bagaces, Bagaces
-Bagaces, La Fortuna
-Bagaces, Mogote
-Bagaces, Río Naranjo
-Carrillo, Filadelfia
-Carrillo, Palmira
-Carrillo, Sardinal
-Carrillo, Belén
-Cañas, Cañas
-Cañas, Palmira
-Cañas, San Miguel
-Cañas, Bebedero
-Cañas, Porozal
-Abangares, Las Juntas
-Abangares, Sierra
-Abangares, San Juan
-Abangares, Colorado
-Tilarán, Tilarán
-Tilarán, Quebrada Grande
-Tilarán, Tronadora
-Tilarán, Santa Rosa
-Tilarán, Líbano
-Tilarán, Tierras Morenas
-Tilarán, Arenal
-Tilarán, Cabeceras
-Nandayure, Carmona
-Nandayure, Santa Rita
-Nandayure, Zapotal
-Nandayure, San Pablo
-Nandayure, Porvenir
-Nandayure, Bejuco
-La Cruz, La Cruz
-La Cruz, Santa Cecilia
-La Cruz, La Garita
-La Cruz, Santa Elena
-Hojancha, Hojancha
-Hojancha, Monte Romo
-Hojancha, Puerto Carrillo
-Hojancha, Huacas
-Hojancha, Matambú
-Puntarenas, Puntarenas
-Puntarenas, Pitahaya
-Puntarenas, Chomes
-Puntarenas, Lepanto
-Puntarenas, Paquera
-Puntarenas, Manzanillo
-Puntarenas, Guacimal
-Puntarenas, Barranca
-Puntarenas, Monte Verde
-Puntarenas, Isla del Coco
-Puntarenas, Cóbano
-Puntarenas, Chacarita
-Puntarenas, Chira
-Puntarenas, Acapulco
-Puntarenas, El Roble
-Puntarenas, Arancibia
-Esparza, Espíritu Santo
-Esparza, San Juan Grande
-Esparza, Macacona
-Esparza, San Rafael
-Esparza, San Jerónimo
-Esparza, Caldera
-Buenos Aires, Buenos Aires
-Buenos Aires, Volcán
-Buenos Aires, Potrero Grande
-Buenos Aires, Boruca
-Buenos Aires, Pilas
-Buenos Aires, Colinas
-Buenos Aires, Chánguena
-Buenos Aires, Biolley
-Buenos Aires, Brunka
-Montes de Oro, Miramar
-Montes de Oro, La Unión
-Montes de Oro, San Isidro
-Osa, Puerto Cortés
-Osa, Palmar
-Osa, Sierpe
-Osa, Bahía Ballena
-Osa, Piedras Blancas
-Osa, Bahía Drake
-Quepos, Quepos
-Quepos, Savegre
-Quepos, Naranjito
-Golfito, Golfito
-Golfito, Puerto Jiménez
-Golfito, Guaycará
-Golfito, Pavón
-Coto Brus, San Vito
-Coto Brus, Sabalito
-Coto Brus, Aguabuena
-Coto Brus, Limoncito
-Coto Brus, Pittier
-Coto Brus, Gutiérrez Braun
-Parrita, Parrita
-Corredores, Corredor
-Corredores, La Cuesta
-Corredores, Canoas
-Corredores, Laurel
-Garabito, Jacó
-Garabito, Tárcoles
-Garabito, Lagunillas
-Limón, Limón
-Limón, Valle La Estrella
-Limón, Río Blanco
-Limón, Matama
-Pococí, Guápiles
-Pococí, Jiménez
-Pococí, Rita
-Pococí, Roxana
-Pococí, Cariari
-Pococí, Colorado
-Pococí, La Colonia
-Siquirres, Siquirres
-Siquirres, Pacuarito
-Siquirres, Florida
-Siquirres, Germania
-Siquirres, El Cairo
-Siquirres, Alegría
-Siquirres, Reventazón
-Talamanca, Bratsi
-Talamanca, Sixaola
-Talamanca, Cahuita
-Talamanca, Telire
-Matina, Matina
-Matina, Batán
-Matina, Carrandi
-Guácimo, Guácimo
-Guácimo, Mercedes
-Guácimo, Pocora
-Guácimo, Río Jiménez
-Guácimo, Duacarí
+let data = `Bogotá, Bogotá D.C.
+Medellín, Antioquia
+Cali, Valle del Cauca
+Barranquilla, Atlántico
+Cartagena, Bolívar
+Cúcuta, Norte de Santander
+Bucaramanga, Santander
+Pereira, Risaralda
+Santa Marta, Magdalena
+Manizales, Caldas
+Ibagué, Tolima
+Villavicencio, Meta
+Sincelejo, Sucre
+Montería, Córdoba
+Neiva, Huila
+Armenia, Quindío
+Valledupar, Cesar
+Pasto, Nariño
+San Andrés, San Andrés y Providencia
+Popayán, Cauca
+Tunja, Boyacá
+Quibdó, Chocó
+Leticia, Amazonas
+Mocoa, Putumayo
+Yopal, Casanare
+Riohacha, La Guajira
+Florencia, Caquetá
+Margarita, Guainía
+San José del Guaviare, Guaviare
+Arauca, Arauca
+Inírida, Guainía
+Vaupés, Vaupés
+Mitú, Vaupés
+Pto Inírida, Guainía
+La Dorada, Caldas
+Sogamoso, Boyacá
+Zipaquirá, Cundinamarca
+Soacha, Cundinamarca
+Chía, Cundinamarca
+Facatativá, Cundinamarca
+Fusagasugá, Cundinamarca
+Girardot, Cundinamarca
+Tocancipá, Cundinamarca
+Cajicá, Cundinamarca
+Mosquera, Cundinamarca
+Cota, Cundinamarca
+Sibaté, Cundinamarca
+Rionegro, Antioquia
+Envigado, Antioquia
+Itagüí, Antioquia
+Sabaneta, Antioquia
+Turbo, Antioquia
+Apartadó, Antioquia
+Valdivia, Antioquia
+San Carlos, Antioquia
+Carmen de Viboral, Antioquia
+La Ceja, Antioquia
+La Unión, Antioquia
+Rionegro, Antioquia
+Pueblorrico, Antioquia
+Marinilla, Antioquia
+El Retiro, Antioquia
+Santa Rosa de Osos, Antioquia
+Segovia, Antioquia
+Yarumal, Antioquia
+Tolu, Sucre
+Sincelejo, Sucre
+La Unión, Nariño
+Samaniego, Nariño
+Pasto, Nariño
+Chachagüí, Nariño
+Ipiales, Nariño
+Cumbal, Nariño
+Guapí, Cauca
+Popayán, Cauca
+Páez, Cauca
+El Bordo, Cauca
+Timbiquí, Cauca
+Santiago de Cali, Valle del Cauca
+Palmira, Valle del Cauca
+Buenaventura, Valle del Cauca
+Tuluá, Valle del Cauca
+Buga, Valle del Cauca
+Candelaria, Valle del Cauca
+Yumbo, Valle del Cauca
+Cartago, Valle del Cauca
+Roldanillo, Valle del Cauca
+La Victoria, Valle del Cauca
+El Cerrito, Valle del Cauca
+Jamundí, Valle del Cauca
+Cali, Valle del Cauca
+Ginebra, Valle del Cauca
+Guacarí, Valle del Cauca
+Vélez, Santander
+Barichara, Santander
+Girón, Santander
+Barrancabermeja, Santander
+San Gil, Santander
+Bucaramanga, Santander
+Floridablanca, Santander
+Piedecuesta, Santander
+Málaga, Santander
+Sabana de Torres, Santander
+Puerto Wilches, Santander
+Chiquinquirá, Boyacá
+Duitama, Boyacá
+Sogamoso, Boyacá
+Tunja, Boyacá
+Berbeo, Boyacá
+Paipa, Boyacá
+Monguí, Boyacá
+Somondoco, Boyacá
+Tibaná, Boyacá
+Ramiriquí, Boyacá
+Sáchica, Boyacá
+Villanueva, Boyacá
+Sutamarchán, Boyacá
+Chiquinquirá, Boyacá
+Turmequé, Boyacá
+Vélez, Santander
+Barichara, Santander
+San Gil, Santander
+Bucaramanga, Santander
+Floridablanca, Santander
+Girón, Santander
+Lebrija, Santander
+Málaga, Santander
+Piedecuesta, Santander
+Barrancabermeja, Santander
+San Vicente de Chucurí, Santander
+El Playón, Santander
+Zapatoca, Santander
+Aguachica, Cesar
+Valledupar, Cesar
+La Paz, Cesar
+Bosconia, Cesar
+Riohacha, La Guajira
+Maicao, La Guajira
+Fonseca, La Guajira
+La Jagua de Ibirico, La Guajira
+San Juan del Cesar, La Guajira
+Villanueva, La Guajira
+Samué, La Guajira
+Dibulla, La Guajira
+Piohacha, La Guajira
+Ciénaga, Magdalena
+Santa Marta, Magdalena
+Aracataca, Magdalena
+El Banco, Magdalena
+Fundación, Magdalena
+García, Magdalena
+Magangué, Bolívar
+Cartagena, Bolívar
+Turbaná, Bolívar
+Turbaco, Bolívar
+Mompox, Bolívar
+San Jacinto, Bolívar
+María la Baja, Bolívar
+San Juan Nepomuceno, Bolívar
+Arjona, Bolívar
+Luruaco, Atlántico
+Barranquilla, Atlántico
+Soledad, Atlántico
+Malambo, Atlántico
+Sabanalarga, Atlántico
+Puerto Colombia, Atlántico
+Juan de Acosta, Atlántico
+Galapa, Atlántico
+Santo Tomás, Atlántico
+Piohacha, La Guajira
+San Juan del Cesar, La Guajira
+Fonseca, La Guajira
+La Jagua de Ibirico, La Guajira
+Hatonuevo, La Guajira
+San José de los Caños, La Guajira
 `;
 
 export const cities = data.split('\n');
